435a19dede96bf3d147c6c6f92447e6436c40cee
jtodic <51692139+jtodic@users.noreply.github.com>
2022-09-25 06:16:26 +0200

    added comments

--- entrypoint.py
+++ entrypoint.py
@@ -3,10 +3,9 @@ import sys
 
 SERVICE1_URL = "http://service1:8080"
 
-#http://api.open-notify.org/astros.json
-
 message = requests.get(sys.stdin.readline()).text #sys.stdin.readline()
 data = ["md5", message]
+
 print (data)
 print(requests.post(SERVICE1_URL, data="\n".join(data)))
 print(requests.post(SERVICE1_URL, data="\n".join(data)).status_code == requests.codes.ok)
